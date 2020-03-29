20f0f64b5a3d8c5ea606fe588260b403d22d4339
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-03-29 22:58:26 +0000

    Added Modal and cleaned up codes.

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-document.getElementById('get-quote').addEventListener('click', go);
+document.getElementById('get-quote').addEventListener('click', getQoute);
 const showcase = document.getElementById('showcase');
 
-function go() {
-  console.log('mee');
-  // showcase.innerHTML = hello;
-  fetch('https://jsonplaceholder.typicode.com/posts')
+function getQoute() {
+  fetch('https://type.fit/api/quotes')
     .then(res => res.json())
     .then(data => {
       const num = Math.floor(Math.random() * 100);
-      console.log(num);
       const oneQuote = data[num];
-      const ouput = `<p class = 'output' id='output'>${oneQuote.body}</p>`;
+      const ouput = `<p class = 'output' id='output'>${oneQuote.text}</p>
+      <p><cite>~ ${
+        oneQuote.author
+          ? oneQuote.author
+          : 'Unknown, probably a great fella <span>😅</span>'
+      }</cite></p>`;
       showcase.innerHTML = ouput;
     });
 }
@@ -27,9 +29,36 @@ const checkbox = document
     document.getElementById('showcase').classList.toggle('dark');
   });
 
+//Dynamic Footer Date
 const date = document.getElementById('date');
 const year = new Date().getFullYear();
 
-console.log(year);
-
+//Insert Dynamic Date in footer
 date.innerHTML = `<p>&copy ${year}</p>`;
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('myBtn');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
